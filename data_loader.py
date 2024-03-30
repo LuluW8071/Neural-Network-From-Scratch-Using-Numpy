11bc703f254d4881c76001bd83c8e75f1b0505e6
luluw <shashinmhrzn@gmail.com>
2024-03-30 23:24:01 +0545

    :zap: [FIX] One Hot Encoder

--- data_loader.py
+++ data_loader.py
@@ -1,8 +1,13 @@
 import pandas as pd
+import numpy as np
 
 def load_data(train_path, test_path):
     train = pd.read_csv(train_path)
     X_test = pd.read_csv(test_path)
     X_train = train.drop('label', axis=1)
-    y_train = train['label']
-    return X_train, y_train, X_test
+    labels = train['label']
+    num_classes = labels.max() + 1
+    y_train = np.eye(num_classes)[labels]
+    y_train = y_train.astype(int)
+
+    return X_train, y_train, X_test, num_classes
