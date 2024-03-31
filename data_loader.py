81d38e2a1b8e07dd6e9d949a6d69c3c27fb889db
luluw <shashinmhrzn@gmail.com>
2024-03-31 12:28:02 +0545

    :zap: [FIX] One hot encode

--- data_loader.py
+++ data_loader.py
@@ -1,7 +1,22 @@
 import pandas as pd
 import numpy as np
 
+
 def load_data(train_path, test_path):
+    """
+    Function to load data and one hot encode.
+
+    Args:
+        train_path (str): File path to the training data CSV file.
+        test_path (str): File path to the test data CSV file.
+
+    Returns:
+        tuple: A tuple containing four elements:
+            - X_train (DataFrame): Training features.
+            - y_train (ndarray): One-hot encoded training labels.
+            - X_test (DataFrame): Test features.
+            - num_classes (int): Number of classes in the dataset.
+    """
     train = pd.read_csv(train_path)
     X_test = pd.read_csv(test_path)
     X_train = train.drop('label', axis=1)
